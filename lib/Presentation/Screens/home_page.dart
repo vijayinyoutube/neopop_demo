c8b6f6d3aa1c34e9e723a2d968c955a2801fc990
vijayinyoutube <vijaythevirtue@gmail.com>
2023-05-17 12:17:42 +0530

    refactor

--- lib/Presentation/Screens/home_page.dart
+++ lib/Presentation/Screens/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:neopop/neopop.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
@@ -7,12 +8,35 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("assets/Images/bg_image.png"),
-            ),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/Images/bg_image.png"),
           ),
-          child: null),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(50.0),
+          child:
+              Align(alignment: Alignment.bottomCenter, child: buildNeopopBtn()),
+        ),
+      ),
     );
   }
+
+  Widget buildNeopopBtn() => NeoPopTiltedButton(
+        isFloating: true,
+        onTapUp: () => debugPrint('Play now'),
+        decoration: const NeoPopTiltedButtonDecoration(
+          color: Color(0xFFffe22d),
+          plunkColor: Color(0xFFc3a13b),
+          shadowColor: Colors.black,
+        ),
+        child: const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 70.0, vertical: 15),
+          child: Text('Play Now →',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold)),
+        ),
+      );
 }
