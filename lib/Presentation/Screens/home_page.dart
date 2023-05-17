3ce4dee2469e13d6ad7bb8a1fc624469cd6b317a
vijayinyoutube <vijaythevirtue@gmail.com>
2023-05-17 11:59:51 +0530

    initial commit

---
+++ lib/Presentation/Screens/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/Images/bg_image.png"),
+            ),
+          ),
+          child: null),
+    );
+  }
+}
